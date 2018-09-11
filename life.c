cc9cbe9ab9bee575507ee14bc08f401daa17c3a4
George McCarron <hello@georgemccarron.com>
2018-09-11 22:06:59 +0100

    set up the grid

--- life.c
+++ life.c
@@ -3,9 +3,15 @@
 /* Implements rules as stated at: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
+    int rows = 20;
+    int cols = 60;
+    int* grid = (int*) calloc(rows*cols, rows * cols * sizeof(int));
     
+    
+    free(grid);
     return 0;
 }
