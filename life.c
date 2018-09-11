8f4af43819283cf92539f88e2eecaf479cae8174
George McCarron <hello@georgemccarron.com>
2018-09-11 22:38:53 +0100

    added function to print the grid

--- life.c
+++ life.c
@@ -16,12 +16,28 @@ void setCell(int* grid, int x, int y, int val) {
     *(grid + y*cols + x) = val;
 }
 
+void printGrid(int* grid) {
+    int i, j;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            if (getCell(grid, j, i) == 0) {
+                printf(" ");
+            } else {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char* argv[]) {
     int* grid = (int*) calloc(rows*cols, rows * cols * sizeof(int));
-    
+
     setCell(grid, 1, 1, 1);
     setCell(grid, 1, 2, 1);
     setCell(grid, 1, 3, 1);
+
+    printGrid(grid);
 
     free(grid);
     return 0;
