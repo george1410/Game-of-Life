14c5feb43b3632639858c66f95037dbb6a2a82d8
George McCarron <hello@georgemccarron.com>
2018-09-11 21:46:43 +0100

    initial commit

---
+++ life.c
@@ -0,0 +1,11 @@
+/* Implementation of Conway's Game of Life ('Life') */
+/* George McCarron 2018 */
+/* Implements rules as stated at: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life */
+
+#include <stdio.h>
+
+main(int argc, char* argv[])
+{
+    
+    return 0;
+}
